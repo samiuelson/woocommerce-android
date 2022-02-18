c0f737466d0584d5123117f756b0cbcc1fd2947e
malinajirka <malinajirka@gmail.com>
2022-02-18 15:37:10 +0100

    Add logging

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductType.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/products/ProductType.kt
@@ -2,6 +2,8 @@ package com.woocommerce.android.ui.products
 
 import androidx.annotation.StringRes
 import com.woocommerce.android.R
+import com.woocommerce.android.util.WooLog
+import com.woocommerce.android.util.WooLog.T
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductType
 
 enum class ProductType(@StringRes val stringResource: Int = 0, val value: String = "") {
@@ -15,6 +17,7 @@ enum class ProductType(@StringRes val stringResource: Int = 0, val value: String
     companion object {
         fun fromCoreProductType(coreProductType: String, isVirtual: Boolean): ProductType {
             if (isVirtual && coreProductType.lowercase() != "simple") {
+                WooLog.w(T.PRODUCTS, "Unknown productType state: isVirtual == true but type is $coreProductType")
                 // Virtual non-simple products are not known to the app => OTHER is returned
                 return OTHER
             }
