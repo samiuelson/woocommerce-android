cb47f277c779705530cb0b8297969b433e9635e5
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-04-29 06:26:43 -0400

    Refactoring the Card Reader Manuals string to keep it consistent

--- WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/hub/CardReaderManualsScreen.kt
+++ WooCommerce/src/main/kotlin/com/woocommerce/android/ui/prefs/cardreader/hub/CardReaderManualsScreen.kt
@@ -3,20 +3,20 @@ package com.woocommerce.android.ui.prefs.cardreader.hub
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.woocommerce.android.R
 
 @Composable
 fun ManualsScreen(
@@ -45,7 +45,7 @@ fun ManualListItem(
         Image(
             painterResource(manualIcon),
             contentDescription = null,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
         )
         Column(
             modifier = Modifier
@@ -73,6 +73,12 @@ fun ManualsList (
                 manualLabel = manual.label,
                 manualIcon = manual.icon,
                 onManualClick = { /*TODO*/ })
+            Divider(
+                modifier = Modifier
+                    .offset(x = 96.dp),
+                color = colorResource(id = R.color.divider_color),
+                thickness = 1.dp
+            )
         }
     }
 }
